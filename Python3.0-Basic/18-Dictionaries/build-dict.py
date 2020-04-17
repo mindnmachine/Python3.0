b3b5ffdaa32e27dd4ceb3eee58dc5528f8462289
Pravin Bhandarkar <pravin@rtbrick.com>
2020-04-17 06:38:12 +0530

    My Directories

---
+++ Python3.0-Basic/18-Dictionaries/build-dict.py
@@ -0,0 +1,28 @@
+ #python3 build-dict.py
+
+print("Method 1: Building Dictionaries")
+build_dictionary = {}
+for i in range(0, 2):
+    dic_key = input("Enter key ")
+    dic_val = input("Enter val ")
+    build_dictionary.update({dic_key: dic_val})
+print(f"Dictionary is {build_dictionary}")
+    
+print("Method 2: Building Dictionaries")
+build_dictionary = {}
+for i in range(0, 2):
+    dic_key = input("Enter key ")
+    dic_val = input("Enter val ")
+    build_dictionary[dic_key] = dic_val
+
+print(f"Dictionary is {build_dictionary}")
+   
+print(f"Method 3: Building Dictionaries")
+build_dictionary = {}
+i = 0
+while i < 2:
+    dict_key = input("Enter key ")
+    dict_val = input("Enter val ")
+    build_dictionary.update({dict_key: dict_val})
+    i = i + 1
+print(f"Dictionary is {build_dictionary}")
