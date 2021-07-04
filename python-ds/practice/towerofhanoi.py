64325e581297282dd23fdcf14314406e1ccc52a7
Pravin S Bhandarkar <pravin@rtbrick.com>
2021-07-04 15:33:48 +0530

    Tower of Hanoi -II

--- python-ds/practice/towerofhanoi.py
+++ python-ds/practice/towerofhanoi.py
@@ -3,10 +3,10 @@ import sys
 def towers(num,src,dest, aux):
     
     if num == 1:
-        print('Move disk 1 from pole %s to pole %s' %(src,dest))
+        print('Move disk 1 from rod %s to rod %s' %(src,dest))
         return
     towers(num-1, src,aux,dest)
-    print ('Move disk %i from pole %s to pole %s'%(num,src,dest))
+    print ('Move disk %i from rod %s to rod %s'%(num,src,dest))
     towers(num-1, aux,dest,src)        
         
 
